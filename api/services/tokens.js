7f147f7e5fe5cb68c6640c982453aa870c912097
rahuldamodar94 <rahuldamodar94@gmail.com>
2021-04-20 11:58:21 +0530

    fix: lower case typo

--- api/services/tokens.js
+++ api/services/tokens.js
@@ -137,7 +137,7 @@ class TokenService {
           external_url: params.external_url,
           attributes: params.attributes,
           name: params.name,
-          name_lowercase: params.name.toLoweCase(),
+          name_lowercase: (params.name).toLowerCase(),
           categories: { connect: { id: parseInt(params.category_id) } },
         },
       });
