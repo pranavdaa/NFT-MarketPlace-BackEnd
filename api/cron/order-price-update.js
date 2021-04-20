a875f6f5ae35ff9c40217c31454b856ac11b1f5b
fuzious <arpitsrivastava2012@gmail.com>
2021-04-20 19:14:54 +0530

    usd_prices cron optimised

--- api/cron/order-price-update.js
+++ api/cron/order-price-update.js
@@ -2,23 +2,10 @@ const cron = require("node-cron");
 const orderService = require("../services/order");
 let orderServiceInstance = new orderService();
 let helper = require("../utils/helper");
+const prisma = require("../../prisma");
 
 cron.schedule("*/59 * * * * *", async function () {
-  let orderList = await orderServiceInstance.getOrderList();
-  let weth_price = await helper.getRate("WETH");
-  let dai_price = await helper.getRate("DAI");
-
-  for (order of orderList) {
-    let price;
-    if (order.erc20tokens.symbol === "WETH") {
-      price = weth_price;
-    } else {
-      price = dai_price;
-    }
-
-    await orderServiceInstance.updateOrderPrice({
-      orderId: order.id,
-      usdPrice: price * order.price,
-    });
-  }
+  // updates order usd_prices by mulitplying prices and their corresponding crypto value from erc20tokens tables
+  const result = await prisma.raw('update mainnet.orders O set usd_price = subquery.usd_price from (select cast(O.price AS FLOAT) * cast(E.market_price AS FLOAT) as usd_price, O.id as order_id from mainnet.orders O inner join mainnet.erc20tokens E ON O.erc20tokens_id =E.id) as subquery where O.id = subquery.order_id;');
+  console.log('cron order_usd_prices : no. of rows updated = '+result);
 });
