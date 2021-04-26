b52e3835bd0078de5a59f83e7393a468e7c71d50
rahuldamodar94 <rahuldamodar94@gmail.com>
2021-04-26 14:09:32 +0530

    fix: config api response format

--- api/middlewares/verify-token-config.js
+++ api/middlewares/verify-token-config.js
@@ -8,19 +8,33 @@ async function verifyToken(req, res, next) {
   var token = req.headers["x-access-token"] || req.headers["authorization"];
 
   if (!token) {
-    return res.status(200).json({ message: constants.MESSAGES.UNAUTHORIZED , isAuthenticated: false  });
+    return res
+      .status(200)
+      .json({
+        message: constants.MESSAGES.UNAUTHORIZED,
+        data: { isAuthenticated: false },
+      });
   }
 
   jwt.verify(token, config.secret, async (err, decoded) => {
     if (err) {
-      return res.status(200).json({ message: constants.MESSAGES.UNAUTHORIZED , isAuthenticated: false });
+      return res
+        .status(200)
+        .json({
+          message: constants.MESSAGES.UNAUTHORIZED,
+          data: { isAuthenticated: false },
+        });
     }
+
     if (!decoded.username) {
       let user = await userServiceInstance.getUser(decoded);
       if (!user) {
         return res
           .status(200)
-          .json({ message: constants.MESSAGES.UNAUTHORIZED , isAuthenticated: false });
+          .json({
+            message: constants.MESSAGES.UNAUTHORIZED,
+            data: { isAuthenticated: false },
+          });
       }
       req.userId = decoded.userId;
     }
